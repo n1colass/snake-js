6a5c3cfb5c767cee618c20063d8bca96388dcd77
Nicolas <76406182+n1colass@users.noreply.github.com>
2022-06-20 22:45:00 +0300

    add highest score
    add highest score( localStorage )

--- js/script.js
+++ js/script.js
@@ -5,25 +5,28 @@ const height = canv.height;
 const width = canv.width;
 const sizeCoin = 4;
 const sizeSnake = 16;
-let scoreboard = 0;
 
+if( localStorage.highestScore === undefined ) localStorage.setItem('highestScore','0');
+document.querySelector('.highest-score').innerHTML = `${localStorage.getItem('highestScore')}`;
+
+let scoreboard = 0;
 let snake = [];
-snake[0] = {
-    x:(width/2)-sizeSnake/2,
-    y:(height/2)-sizeSnake/2,
-}
 let coin = {
     x:0,
     y:0,
 }
-function coordCoin(){
-    coin.x = getRandomInt(0, width / sizeSnake ) * sizeSnake;
-    coin.y = getRandomInt(0, height / sizeSnake ) * sizeSnake ;
+
+snake[0] = {
+    x:(width/2)-sizeSnake/2,
+    y:(height/2)-sizeSnake/2,
 }
+
 document.addEventListener("keydown",moveDirection);
+
 // control directions
 let currentDirection = null;
 let queue = [];
+
 function moveDirection(event){
     if(event.keyCode == 37 && currentDirection != "right" && currentDirection != "left" ) queue.unshift("left");
     else if(event.keyCode == 39 && currentDirection != "left" && currentDirection != "right") queue.unshift("right");
@@ -35,50 +38,54 @@ spawnCoin();
 drawSnake();
 
 function drawGame(){
-
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
+
     drawCoin();
+
     // refresh game
     for(let i = 1; i < snake.length; i++){
         if( snakeX == snake[i].x && snakeY == snake[i].y ){
             document.location.reload();
+            document.querySelector('.highest-score').innerHTML = `${localStorage.getItem('highestScore')}`;
         }
     }
+
     // control snake
     if (queue.length){
         if(queue[queue.length - 1] != currentDirection){
             currentDirection = queue[queue.length - 1];
         }
     }
-        if(currentDirection == "left"){
-            snakeX -= sizeSnake;
-             for(let i = 0; i < snake.length; i++){
-                 ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
-             }
-             queue.pop();
-         } 
-         if(currentDirection == "right"){
-             snakeX += sizeSnake;
-             for(let i = 0; i < snake.length; i++){
-                 ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
-             }
-             queue.pop();
-         } 
-         if(currentDirection == "up"){
-             snakeY -= sizeSnake;
-             for(let i = 0; i < snake.length; i++){
-                 ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
-             }
-             queue.pop();
-         } 
-         if(currentDirection == "down"){
-             snakeY += sizeSnake;
-             for(let i = 0; i < snake.length; i++){
-                 ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
-             }
-             queue.pop();
-         }
+
+    if(currentDirection == "left"){
+        snakeX -= sizeSnake;
+        for(let i = 0; i < snake.length; i++){
+            ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
+        }
+        queue.pop();
+    } 
+    if(currentDirection == "right"){
+        snakeX += sizeSnake;
+        for(let i = 0; i < snake.length; i++){
+            ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
+        }
+        queue.pop();
+    } 
+    if(currentDirection == "up"){
+        snakeY -= sizeSnake;
+        for(let i = 0; i < snake.length; i++){
+            ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
+        }
+        queue.pop();
+    } 
+    if(currentDirection == "down"){
+        snakeY += sizeSnake;
+        for(let i = 0; i < snake.length; i++){
+            ctx.clearRect(snake[i].x,snake[i].y,sizeSnake,sizeSnake);
+        }
+        queue.pop();
+    }
 
     // border collision
     if( snakeX == 400 ){
@@ -91,6 +98,7 @@ function drawGame(){
     }else if ( snakeY == 0-sizeSnake ){
         snakeY = 400;
     }
+
     // eat coin, spawn new coin, add tail to snake
     function check(callback){
         callback();
@@ -104,9 +112,11 @@ function drawGame(){
         }
     }
     check(repeat);
+
     if( snakeX == coin.x && snakeY == coin.y){
         spawnCoin();
         score();
+        if (scoreboard > localStorage.highestScore) localStorage.setItem('highestScore',scoreboard); // highest score LOCALSTORAGE
     }else{
         snake.pop();
     }
@@ -143,6 +153,11 @@ function drawCoin(){
     ctx.arc(coin.x + sizeSnake / 2,coin.y + sizeSnake / 2 ,sizeCoin,0,10);
     ctx.fill();
     ctx.closePath();
+}
+
+function coordCoin(){
+    coin.x = getRandomInt(0, width / sizeSnake ) * sizeSnake;
+    coin.y = getRandomInt(0, height / sizeSnake ) * sizeSnake ;
 }
 
 function getRandomInt(min,max){
